1d1c41b2c1aefab3271d8abfa8e2ae81a792111d
Lutfilah Ahmad <lutfilahahmad169@gmail.com>
2026-09-20 20:11:42 +0700

    feat: menambah settingan config

---
+++ config/shipping.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'tarif_per_km' => env('SHIPPING_TARIF_PER_KM', 3000),
+    'biaya_penanganan' => env('SHIPPING_BIAYA_PENANGANAN', 5000),
+    'header' => env('SHIPPING_HEADER', 'Layanan Pengiriman Obat SehatNusantara'),
+];
